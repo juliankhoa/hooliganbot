4ebfc1a142d399fee50ac0c7811a49c14fa1c3ec
Julian <72424146+juliankhoa@users.noreply.github.com>
2022-12-15 18:37:04 -0500

    updated presence status

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,9 @@
+const { ActivityType } = require('discord.js');
+
 module.exports = async(client) => {
     client.logger.ready(`Bot is ready! (${client.user.tag})`);
-    client.user.setActivity('you always', { type: 'WATCHING' });
+    client.user.setPresence({
+        activities: [{ name: 'you always', type: ActivityType.Watching }],
+        status: 'online'
+    });
 }
